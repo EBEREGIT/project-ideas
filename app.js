542821f99feb41a5d4581a9686743ff73f0797ce
EBEREGIT <samsoneberenjoku@gmail.com>
2020-07-30 18:47:20 +0100

    seperated the DB configuration to a different file and added .env

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 // external imports
 const express = require("express");
 const mongoose = require("mongoose");
+const dbConnect = require("./database/dbConnect")
 
 // initializations
 const app = express();
 
 // mongoDB connect
-mongoose
-  .connect(
-    "mongodb+srv://Ebere:qWjOLsZTlMxY0uHM@cluster0-ev2sa.mongodb.net/project-ideas?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => {
-    console.log("Successfully connected to MongoDB Atlas!");
-  })
-  .catch((error) => {
-    console.log("Unable to connect to MongoDB Atlas!");
-    console.error(error);
-  });
+dbConnect();
 
 app.use((request, response) => {
   response.json({ message: "Welcome to Project Ideas" });
