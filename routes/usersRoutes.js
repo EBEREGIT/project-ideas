3e896c7d09591beed400f490e829300a66ac7ab8
EBEREGIT <samsoneberenjoku@gmail.com>
2020-07-31 20:56:52 +0100

    readUser (Login) completed, JWT added

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const createUser = require("./usersRoutes/createUser");
+const readUser = require("./usersRoutes/readUser");
 
 router.post("/create-user", createUser.createUser);
+router.post("/read-user", readUser.readUser);
 
 module.exports = router;
