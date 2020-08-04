c83bbf2ba45bd36763f7ccc5a07ca0b70b055bd3
EBEREGIT <samsoneberenjoku@gmail.com>
2020-08-04 23:29:06 +0100

    readUserProjects completed

--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -9,11 +9,13 @@ const editProject = require("./projectsRoutes/editProject");
 const readProject = require("./projectsRoutes/readProject");
 const readAllProjects = require("./projectsRoutes/readAllProjects");
 const deleteProject = require("./projectsRoutes/deleteProject");
+const readUserProjects = require("./projectsRoutes/readUserProjects");
 
 // project routes
 router.post("/create", auth, createProject.createProject);
 router.put("/edit/:id", auth, editProject.editProject);
 router.delete("/delete/:id", auth, deleteProject.deleteProject);
+router.get("/user", auth, readUserProjects.readUserProjects);
 router.get("/read/:id", readProject.readProject);
 router.get("/", readAllProjects.readAllProjects);
 
---
+++ routes/projectsRoutes/readUserProjects.js
@@ -0,0 +1,20 @@
+// internal imports
+const Project = require("../../database/model/projects");
+
+exports.readUserProjects = (request, response) => {
+    Project.find({ uploadedBy: request.user.userId })
+    .then((result) => {
+      // success
+      response.status(200).send({
+        message: "Projects found Successfully",
+        result,
+      });
+    })
+    .catch((error) => {
+      // failure to create user
+      response.status(404).send({
+        message: "Project Not found",
+        error,
+      });
+    });
+}
