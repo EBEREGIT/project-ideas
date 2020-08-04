b6e8f30ad08fc17b98a5becfeb07dac81aa3de1c
EBEREGIT <samsoneberenjoku@gmail.com>
2020-08-04 22:37:46 +0100

    readAllProjects completed

--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -7,10 +7,12 @@ const auth = require("../middleware/auth");
 const createProject = require("./projectsRoutes/createProject");
 const editProject = require("./projectsRoutes/editProject");
 const readProject = require("./projectsRoutes/readProject");
+const readAllProjects = require("./projectsRoutes/readAllProjects");
 
 // project routes
 router.post("/create", auth, createProject.createProject);
 router.put("/edit/:id", auth, editProject.editProject);
-router.get("/read/:id", auth, readProject.readProject);
+router.get("/read/:id", readProject.readProject);
+router.get("/", readAllProjects.readAllProjects);
 
 module.exports = router;
