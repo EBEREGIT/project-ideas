559724d9c8ba73f283c07c46820e509902e6ec0b
EBEREGIT <samsoneberenjoku@gmail.com>
2020-08-04 20:50:02 +0100

    create project validation completed

--- routes/projectsRoutes/createProject.js
+++ routes/projectsRoutes/createProject.js
@@ -1,7 +1,23 @@
 // internal imports
-const User = require("../../database/model/users");
+const Project = require("../../database/model/projects");
+const projectValidation = require("../../validatioin/projectsValidation")
 
 exports.createProject = (request, response) => {
-    // const user = User.findById(request.user._id);
-    response.json(request.user);
-}
+    // execute validation query
+  let { error } = projectValidation.createProject.validate(request.body)
+
+  // if there is an error during validation, terminate execution
+  if (error) return response.status(403).send(error.details[0].message);
+
+  response.send(request.body);
+  // create a new instance of the project model and pass in the data
+  const project = new Project({
+    _id: request.body._id,
+    projectName: request.body.projectName,
+    sampleURL: request.body.sampleURL,
+    instructionURL: request.body.instructionURL,
+    otherDetails: request.body.otherDetails,
+    seniority: request.body.seniority,
+    uploadedBy: request.user.userId,
+  });
+};
